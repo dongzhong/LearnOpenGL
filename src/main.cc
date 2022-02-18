e06c3fee6ed1f336857a0c364befda0348f4b1f5
Dong Zhong <dongzhong1990@foxmail.com>
2022-02-18 22:15:15 +0800

    环境搭建

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,67 @@
+// Created by Dong Zhong on 2022/02/18.
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+void ProcessInput(GLFWwindow* window);
+
+void Init();
+
+void Draw();
+
 int main() {
   // TODO:
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr) {
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    return -1;
+  }
+
+  Init();
+
+  while (!glfwWindowShouldClose(window)) {
+    ProcessInput(window);
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    Draw();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
   return 0;
 }
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+void Init() {}
+
+void Draw() {}
