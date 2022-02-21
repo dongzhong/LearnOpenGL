6b31a4485584cfb158b03771f7b87d73092a81de
dongzhong <dongzhong@bilibili.com>
2022-02-21 23:38:44 +0800

    绘制三角形

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,7 @@
 // Created by Dong Zhong on 2022/02/18.
 
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -62,6 +64,106 @@ void ProcessInput(GLFWwindow* window) {
   }
 }
 
-void Init() {}
+const char* kVertexShader = R"__SHADER__(
+#version 330 core
+layout (location = 0) in vec3 pos;
+void main() {
+  gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+}
+)__SHADER__";
 
-void Draw() {}
+const char* kFragmentShader = R"__SHADER__(
+#version 330 core
+out vec4 frag_color;
+void main() {
+  frag_color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+)__SHADER__";
+
+float vertices[] = {
+    0.5f,  0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+    -0.5f,  0.5f, 0.0f,
+};
+
+unsigned int indices[] = { // 注意索引从0开始!
+    0, 1, 3, // 第一个三角形
+    1, 2, 3  // 第二个三角形
+};
+
+GLuint program;
+GLuint vertex_array;
+
+void Init() {
+  // Shader and Program
+  GLint success;
+  char info_log[512];
+
+  GLuint vertex_shader;
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertex_shader, 1, &kVertexShader, nullptr);
+  glCompileShader(vertex_shader);
+
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(vertex_shader, 512, nullptr, info_log);
+    std::cout << "DongZhong: " << "Vertex shader compile error: " << info_log;
+  }
+
+  GLuint fragment_shader;
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  glShaderSource(fragment_shader, 1, &kFragmentShader, nullptr);
+  glCompileShader(fragment_shader);
+
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(fragment_shader, 512, nullptr, info_log);
+    std::cout << "DongZhong: " << "Fragment shader compile error: " << info_log;
+  }
+
+  program = glCreateProgram();
+
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(program, 512, nullptr, info_log);
+    std::cout << "DongZhong: " << "Program link error: " << info_log;
+  }
+
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+
+  // Vertex Buffer Object
+  glGenVertexArrays(1, &vertex_array);
+
+  glBindVertexArray(vertex_array);
+
+  GLuint vertex_buffer;
+  glGenBuffers(1, &vertex_buffer);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  GLuint index_buffer;
+  glGenBuffers(1, &index_buffer);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glUseProgram(program);
+}
+
+void Draw() {
+  glBindVertexArray(vertex_array);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
